cc2b188e210513e5167987324852d61700ad722d
Cringenut <ihnatbaikou@gmail.com>
2023-07-04 09:20:41 +0200

    LittleShelf Build 0.0.0

---
+++ app/src/main/java/com/example/littleshelf/GroceriesListActivity.java
@@ -0,0 +1,14 @@
+package com.example.littleshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroceriesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groceries_list);
+    }
+}
---
+++ app/src/main/java/com/example/littleshelf/ReceiptsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.littleshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiptsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipts_list);
+    }
+}
