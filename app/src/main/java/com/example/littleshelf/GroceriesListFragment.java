49b6e3b7322438da8ba27e2ffc9026de80957db7
Cringenut <ihnatbaikou@gmail.com>
2023-07-07 15:15:42 +0200

    LittleShelf Build 0.0.0

--- app/src/main/java/com/example/littleshelf/GroceriesListFragment.java
+++ app/src/main/java/com/example/littleshelf/GroceriesListFragment.java
@@ -7,58 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GroceriesListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GroceriesListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ListView lv;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    SearchView searchView;
+    ArrayAdapter<String> adapter;
+    String[] data = {"item1", "item2", "item3"};
 
     public GroceriesListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GroceriesListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GroceriesListFragment newInstance(String param1, String param2) {
-        GroceriesListFragment fragment = new GroceriesListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groceries_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_groceries_list, container, false);
+        lv = (ListView) view.findViewById(R.id.ListView);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
+        return view;
     }
 }
