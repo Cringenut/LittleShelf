c73349060f59fc9aaff76eca31ee4bdf09e8296d
Cringenut <ihnatbaikou@gmail.com>
2023-07-08 20:41:38 +0200

    LittleShelf Build 0.0.0

--- app/src/main/java/com/example/littleshelf/GroceriesListViewAdapter.java
+++ app/src/main/java/com/example/littleshelf/GroceriesListViewAdapter.java
@@ -1,20 +1,11 @@
 package com.example.littleshelf;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 
-import androidx.annotation.NonNull;
+public class GroceriesListViewAdapter extends BaseAdapter {
 
-import com.example.littleshelf.Activities.GroceriesListActivity;
-
-public class GrociesListViewAdapter extends ArrayAdapter {
-    public GrociesListViewAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
-    }
 
     @Override
     public int getCount() {
@@ -23,12 +14,12 @@ public class GrociesListViewAdapter extends ArrayAdapter {
 
     @Override
     public Object getItem(int position) {
-        return position;
+        return null;
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return 0;
     }
 
     @Override
--- app/src/main/java/com/example/littleshelf/ListViewFragment.java
+++ app/src/main/java/com/example/littleshelf/ListViewFragment.java
@@ -7,12 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
-public class GroceriesListFragment extends Fragment {
+public class ListViewFragment extends Fragment {
 
     ListView lv;
 
@@ -20,7 +19,7 @@ public class GroceriesListFragment extends Fragment {
     BaseAdapter adapter;
     String[] data = {"item1", "item2", "item3"};
 
-    public GroceriesListFragment() {
+    public ListViewFragment() {
         // Required empty public constructor
     }
 
@@ -29,7 +28,7 @@ public class GroceriesListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.fragment_groceries_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_view, container, false);
         lv = (ListView) view.findViewById(R.id.ListView);
         //adapter = new GrociesListViewAdapter<String>();
         lv.setAdapter(adapter);
