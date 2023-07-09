4e6cc46c827e79c07ae16deb524569100ffc1693
Cringenut <ihnatbaikou@gmail.com>
2023-07-09 17:42:44 +0200

    LittleShelf Build 0.0.0

--- app/src/main/java/com/example/littleshelf/Activities/GroceriesListActivity.java
+++ app/src/main/java/com/example/littleshelf/Activities/GroceriesListActivity.java
@@ -3,14 +3,26 @@ package com.example.littleshelf.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.littleshelf.GroceriesListViewAdapter;
 import com.example.littleshelf.items.GroceryItem;
 import com.example.littleshelf.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 
@@ -24,19 +36,45 @@ public class GroceriesListActivity extends AppCompatActivity {
 
         loadNavMenu();
 
-        //listView = findViewById(R.id.ListView);
+        Gson gson = new GsonBuilder().registerTypeAdapter(ArrayList.class, new JsonDeserializer<ArrayList>() {
+                    @Override
+                    public ArrayList deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                        ArrayList list = new ArrayList();
+                        JsonArray jsonArray = json.getAsJsonArray();
 
-        ArrayList<GroceryItem> arrayList = new ArrayList<>();
+                        for (JsonElement element : jsonArray) {
+                            // Deserialize each element and add it to the list
+                            Object obj = context.deserialize(element, GroceryItem.class);
+                            list.add(obj);
+                        }
 
-        arrayList.add(new GroceryItem("Cheese"));
-        arrayList.add(new GroceryItem("Banana"));
-        arrayList.add(new GroceryItem("Water"));
-        arrayList.add(new GroceryItem("Ham"));
+                        return list;
+                    }
+                })
+                .create();
+        String json = getPreferences(MODE_PRIVATE).getString("ArrayList", "");
+        ArrayList<GroceryItem> arrayList = gson.fromJson(json, ArrayList.class);
+
 
         GroceriesListViewAdapter groceriesListViewAdapter = new GroceriesListViewAdapter(this, R.layout.list_item, arrayList);
         listView = getSupportFragmentManager().findFragmentById(R.id.ListView).getView().findViewById(R.id.idListView);
         listView.setAdapter(groceriesListViewAdapter);
 
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.buttonAdd);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.add(new GroceryItem("Cheese"));
+                listView.invalidateViews();
+
+                SharedPreferences  mPrefs = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor prefsEditor = mPrefs.edit();
+                Gson gson = new Gson();
+                String json = gson.toJson(arrayList);
+                prefsEditor.putString("ArrayList", json);
+                prefsEditor.apply();
+            }
+        });
     }
     private void loadNavMenu() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
