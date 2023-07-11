848b24366daac1c8b0eefc5eb39ec14ae9f80cf1
Cringenut <ihnatbaikou@gmail.com>
2023-07-11 20:13:39 +0200

    LittleShelf Build 0.0.0

--- app/src/main/java/com/example/littleshelf/items/GroceryItem.java
+++ app/src/main/java/com/example/littleshelf/items/GroceryItem.java
@@ -2,8 +2,11 @@ package com.example.littleshelf.items;
 
 public class GroceryItem {
 
+    private int id;
     private String name;
-    public GroceryItem(String name) {
+
+    public GroceryItem(int id, String name) {
+        this.id = id;
         this.name = name;
     }
 
