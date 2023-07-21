c8bf23d1a3006c1c47a2c1c47c1a6b025f04b977
Cringenut <ihnatbaikou@gmail.com>
2023-07-21 08:17:42 +0200

    LittleShelf Build 0.0.3

--- app/src/main/java/com/example/littleshelf/Activities/GroceriesListActivity.java
+++ app/src/main/java/com/example/littleshelf/Activities/GroceriesListActivity.java
@@ -44,11 +44,6 @@ public class GroceriesListActivity extends AppCompatActivity {
 
         DataBaseHelper dataBaseHelper = new DataBaseHelper(GroceriesListActivity.this);
 
-        /*listView = getSupportFragmentManager().findFragmentById(R.id.listView).getView().findViewById(R.id.idListView);
-        showGroceriesItemsOnListView(dataBaseHelper);
-
-        */
-
         ((FloatingActionButton) findViewById(R.id.buttonCheese)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,69 +53,12 @@ public class GroceriesListActivity extends AppCompatActivity {
                 }
                 else {
                     fragmentTransaction.show(getSupportFragmentManager().findFragmentById(R.id.addItem));
-
-                    ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonAdd))
-                            .setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    String addGroceryItemName = ((TextInputEditText) getSupportFragmentManager()
-                                            .findFragmentById(R.id.addItem).getView()
-                                            .findViewById(R.id.textInputField)).getText().toString();
-
-                                    if (addGroceryItemName.length() > 0) {
-                                        String date = ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate)).getText().toString();
-                                        dataBaseHelper.addOne(new GroceryItem(-1, addGroceryItemName, date));
-                                        showGroceriesItemsOnListView(dataBaseHelper);
-
-                                        ((TextInputEditText) getSupportFragmentManager()
-                                                .findFragmentById(R.id.addItem).getView()
-                                                .findViewById(R.id.textInputField)).setText("");
-
-                                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                                        fragmentTransaction.hide(getSupportFragmentManager().findFragmentById(R.id.addItem));
-                                        fragmentTransaction.commit();
-
-                                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                                        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                                    }
-                                }
-                            });
-
-                    ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate))
-                            .setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-                                @Override
-                                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                                    String date = dayOfMonth + " " + month + " " + year;
-                                    ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate)).setText(date);
-                                }
-                            };
-
-                            Calendar calendar = Calendar.getInstance();
-                            int year = calendar.get(Calendar.YEAR);
-                            int month = calendar.get(Calendar.MONTH);
-                            int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                            int style = AlertDialog.THEME_HOLO_LIGHT;
-                            DatePickerDialog datePickerDialog = new DatePickerDialog(GroceriesListActivity.this, style, dateSetListener, year, month, day);
-
-                            datePickerDialog.show();
-
-                        }
-                    });
+                    fragmentTransaction.commit();
                 }
-                fragmentTransaction.commit();
             }
         });
     }
 
-    private void showGroceriesItemsOnListView(DataBaseHelper dataBaseHelper) {
-        if (listView != null) {
-
-        }
-    }
     private void loadNavMenu() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.GroceriesList);
--- app/src/main/java/com/example/littleshelf/AddGroceryItemFragment.java
+++ app/src/main/java/com/example/littleshelf/AddGroceryItemFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AddGroceryItemFragment extends Fragment {
 
@@ -19,7 +20,67 @@ public class AddGroceryItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_add_grocery_item, container, false);
+        ((Button) v.findViewById(R.id.buttonAdd)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        return inflater.inflate(R.layout.fragment_add_grocery_item, container, false);
+            }
+        });
+        /*((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonAdd))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String addGroceryItemName = ((TextInputEditText) getSupportFragmentManager()
+                                .findFragmentById(R.id.addItem).getView()
+                                .findViewById(R.id.textInputField)).getText().toString();
+
+                        if (addGroceryItemName.length() > 0) {
+                            String date = ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate)).getText().toString();
+                            dataBaseHelper.addOne(new GroceryItem(-1, addGroceryItemName, date));
+                            showGroceriesItemsOnListView(dataBaseHelper);
+
+                            ((TextInputEditText) getSupportFragmentManager()
+                                    .findFragmentById(R.id.addItem).getView()
+                                    .findViewById(R.id.textInputField)).setText("");
+
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            fragmentTransaction.hide(getSupportFragmentManager().findFragmentById(R.id.addItem));
+                            fragmentTransaction.commit();
+
+                            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                        }
+                    }
+                });
+
+        ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                String date = dayOfMonth + " " + month + " " + year;
+                                ((Button) getSupportFragmentManager().findFragmentById(R.id.addItem).getView().findViewById(R.id.buttonDate)).setText(date);
+                            }
+                        };
+
+                        Calendar calendar = Calendar.getInstance();
+                        int year = calendar.get(Calendar.YEAR);
+                        int month = calendar.get(Calendar.MONTH);
+                        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                        int style = AlertDialog.THEME_HOLO_LIGHT;
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(GroceriesListActivity.this, style, dateSetListener, year, month, day);
+
+                        datePickerDialog.show();
+
+                    }
+                });
+    }*/
+
+
+        return v;
     }
 }
