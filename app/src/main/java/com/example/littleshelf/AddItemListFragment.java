8ffe5a4d523e1c4dbcf45975e298b5e2ed447941
Cringenut <ihnatbaikou@gmail.com>
2023-08-16 11:08:00 +0200

    LittleShelf Build 0.3.0

---
+++ app/src/main/java/com/example/littleshelf/AddItemListFragment.java
@@ -0,0 +1,23 @@
+package com.example.littleshelf;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddItemListFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.d_fragment_add_item_list, container, false);
+    }
+}
