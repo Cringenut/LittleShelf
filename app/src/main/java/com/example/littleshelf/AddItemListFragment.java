bb3c378aadbc8550d7ce35a6f156efab106bc23b
Cringenut <ihnatbaikou@gmail.com>
2023-08-16 20:27:23 +0200

    LittleShelf Build 0.3.0

--- app/src/main/java/com/example/littleshelf/AddItemListFragment.java
+++ app/src/main/java/com/example/littleshelf/AddItemListFragment.java
@@ -6,15 +6,19 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AddItemListFragment extends Fragment {
 
     private Button btnBack;
     private FragmentManager fragmentManager;
+    private TextView searchBar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +35,24 @@ public class AddItemListFragment extends Fragment {
             FragmentTransaction fragmentBackTransaction = fragmentManager.beginTransaction();
             fragmentBackTransaction.remove(this);
             fragmentBackTransaction.commit();
+        });
+
+        searchBar = view.findViewById(R.id.searchBarText);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
 
         return view;
---
+++ app/src/main/java/com/example/littleshelf/SearchBarFragment.java
@@ -0,0 +1,33 @@
+package com.example.littleshelf;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SearchBarFragment extends Fragment {
+
+    private TextView searchBar;
+
+    public SearchBarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.d_fragment_search_bar, container, false);
+    }
+}
