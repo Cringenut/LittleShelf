b0b54501d9b3b8ddb6afac77dba7b3760939d49e
Cringenut <ihnatbaikou@gmail.com>
2023-08-17 21:37:47 +0200

    LittleShelf Build 0.3.0

---
+++ app/src/main/java/com/example/littleshelf/GroceryItemsRecyclerViewFragment.java
@@ -0,0 +1,17 @@
+package com.example.littleshelf;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class GroceryItemsRecyclerViewFragment extends RecyclerView {
+
+    public GroceryItemsRecyclerViewFragment(@NonNull Context context) {
+        super(context);
+    }
+}
