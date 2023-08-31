0250b5f3f68da9d42ccae664b317a85a2e1ae914
Cringenut <ihnatbaikou@gmail.com>
2023-08-31 15:10:51 +0200

    LittleShelf Build 0.3.3

---
+++ app/src/main/java/com/example/littleshelf/HomePage.java
@@ -0,0 +1,14 @@
+package com.example.littleshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hp_activity_home_page);
+    }
+}
