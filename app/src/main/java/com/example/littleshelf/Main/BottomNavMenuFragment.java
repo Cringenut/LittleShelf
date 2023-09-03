9d52543801034cfbe240cff56b3d79d1357898f9
Cringenut <ihnatbaikou@gmail.com>
2023-09-03 20:53:53 +0200

    LittleShelf Build 0.3.3

--- app/src/main/java/com/example/littleshelf/Main/BottomNavMenuFragment.java
+++ app/src/main/java/com/example/littleshelf/Main/BottomNavMenuFragment.java
@@ -2,13 +2,19 @@ package com.example.littleshelf.Main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.littleshelf.Groceries.GroceriesActivity;
+import com.example.littleshelf.HomePage.HomePage;
 import com.example.littleshelf.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class BottomNavMenuFragment extends Fragment {
@@ -22,6 +28,30 @@ public class BottomNavMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment_bottom_nav_menu, container, false);
+        View v = inflater.inflate(R.layout.main_fragment_bottom_nav_menu, container, false);
+
+        BottomNavigationView bottomNavigationView = v.findViewById(R.id.bottomNawMenu);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+            if (id == R.id.Homepage && requireActivity().getClass() != HomePage.class) {
+
+            }
+            else if (id == R.id.Groceries && requireActivity().getClass() != GroceriesActivity.class) {
+
+            }
+            else if (id == R.id.Receipts) {
+
+            }
+            else if (id == R.id.Profile) {
+
+            }
+
+            return true;
+        });
+
+
+
+        return v;
     }
 }
