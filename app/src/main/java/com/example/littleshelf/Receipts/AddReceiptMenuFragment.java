17cefd53e44529edf76efb3718a6bf759fe6ba6a
Cringenut <ihnatbaikou@gmail.com>
2023-09-10 20:28:19 +0200

    LittleShelf Build 0.3.4

---
+++ app/src/main/java/com/example/littleshelf/Receipts/AddReceiptMenuFragment.java
@@ -0,0 +1,22 @@
+package com.example.littleshelf.Receipts;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.littleshelf.R;
+
+public class AddReceiptMenuFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.rec_fragment_add_receipt_menu, container, false);
+    }
+}
