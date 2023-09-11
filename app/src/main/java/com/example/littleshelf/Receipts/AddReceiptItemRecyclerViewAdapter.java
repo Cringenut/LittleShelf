ed08539aca31b5d5c8d71f190f7b307874aa39c7
Cringenut <ihnatbaikou@gmail.com>
2023-09-11 21:12:34 +0200

    LittleShelf Build 0.3.5

---
+++ app/src/main/java/com/example/littleshelf/Receipts/AddReceiptItemRecyclerViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.littleshelf.Receipts;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.littleshelf.R;
+
+public class AddReceiptItemRecyclerViewAdapter extends RecyclerView.Adapter<AddReceiptItemRecyclerViewAdapter.RecyclerViewHolder> {
+
+    private Receipt receipt;
+
+    public AddReceiptItemRecyclerViewAdapter(Receipt receipt) {
+        this.receipt = receipt;
+    }
+
+    protected class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        TextView additionDateItemAmount;
+        private RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            additionDateItemAmount = itemView.findViewById(R.id.textViewAdditionDateItemAmount);
+        }
+    }
+
+    @NonNull
+    @Override
+    public AddReceiptItemRecyclerViewAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                if (viewType == 0) {
+            return new AddReceiptItemRecyclerViewAdapter.RecyclerViewHolder(inflater.inflate(R.layout.main_fragment_grocery_item, parent, false));
+        }
+        return new AddReceiptItemRecyclerViewAdapter.RecyclerViewHolder(inflater.inflate(R.layout.rec_button_add_receipt_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+        if (position == receipt.getReceiptItems().size()) {
+            return;
+        }
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return  (position == receipt.getReceiptItems().size()) ? 1 : 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return receipt.getReceiptItems().size();
+    }
+}
--- app/src/main/java/com/example/littleshelf/Receipts/ReceiptsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/littleshelf/Receipts/ReceiptsRecyclerViewAdapter.java
@@ -23,34 +23,21 @@ public class ReceiptsRecyclerViewAdapter extends RecyclerView.Adapter<ReceiptsRe
 
     protected class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
-        TextView additionDateItemAmount;
-        private RecyclerViewHolder(@NonNull View itemView) {
+        TextView additionDateItemAmount;        private RecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
             additionDateItemAmount = itemView.findViewById(R.id.textViewAdditionDateItemAmount);
         }
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return  (position == receipts.size()) ? 1 : 0;
     }
 
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-
-        if (viewType == 0) {
-            return new ReceiptsRecyclerViewAdapter.RecyclerViewHolder(inflater.inflate(R.layout.rec_fragment_receipt, parent, false));
-        }
         return new ReceiptsRecyclerViewAdapter.RecyclerViewHolder(inflater.inflate(R.layout.rec_button_add_receipt_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReceiptsRecyclerViewAdapter.RecyclerViewHolder holder, int position) {
-        if (position == receipts.size()) {
-            return;
-        }
         holder.additionDateItemAmount.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
     }
 
