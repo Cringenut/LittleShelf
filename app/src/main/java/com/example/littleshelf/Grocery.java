2064bb53de6de342f4ee7664894358b9e431b140
Cringenut <ihnatbaikou@gmail.com>
2023-11-03 22:08:15 +0100

    LittleShelf Build 0.0.1

--- app/src/main/java/com/example/littleshelf/Grocery.java
+++ app/src/main/java/com/example/littleshelf/Grocery.java
@@ -1,4 +1,14 @@
 package com.example.littleshelf;
 
 public class Grocery {
+
+    Grocery(GroceryBuilder builder) {
+
+    }
+
+    public static class GroceryBuilder {
+        public Grocery build() {
+            return new Grocery(this);
+        }
+    }
 }
